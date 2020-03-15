442d965c23b9dc7d77c398f1b6d76a0cfd4890ed
mmezgin <49004855+mmezgin@users.noreply.github.com>
2020-03-15 18:25:38 +0300

    Add files via upload

---
+++ L1consoleLog.js
@@ -0,0 +1,6 @@
+console.log("Hello World :D");
+let a="hi",b="Mars";
+console.log(a,b);
+console.log(2+4);
+console.log("Trying to append a variable: "+b);
+console.log("Trying to append a variable: ",b);
